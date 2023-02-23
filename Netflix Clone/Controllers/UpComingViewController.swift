563ff46fd9a6d2c02c56c9dce3182a07ac176dc8
Yusuf Karan <karan.yusuf.20@gmail.com>
2023-02-24 00:06:40 +0300

    TabBar Created

---
+++ Netflix Clone/Controllers/UpComingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UpComingViewController.swift
+//  Netflix Clone
+//
+//  Created by Yusuf Karan on 23.02.2023.
+//
+
+import UIKit
+
+class UpComingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
